8dec7b0bb1c80f78b93471debffd81e3d928015a
David Lilue <dvdalilue@gmail.com>
2018-05-02 16:48:38 +0200

    Pheromone updating and evaporation

--- ant.erl
+++ ant.erl
@@ -16,47 +16,47 @@ exp() -> [
 
 awakening() ->
     Master = spawn(ant, master, [exp()]),
-    Ants = wakeUp(Master, 10, []),
+    Ants = wakeUp(Master, 100, []),
     {Master, Ants}.
 
-wakeUp(_,0,Ants) when is_list(Ants) -> Ants;
-wakeUp(G,N,Ants) when is_integer(N), is_list(Ants), N > 0 ->
-    A = spawn(ant,ant,[G,1,[]]),
+wakeUp(_, 0, Ants) when is_list(Ants) -> Ants;
+wakeUp(G, N, Ants) when is_integer(N), is_list(Ants), N > 0 ->
+    A = spawn(ant, ant, [G,1,[]]),
     A ! {init},
-    wakeUp(G,N-1,[A|Ants]);
-wakeUp(_,_,_) -> throw("Cannot awake a negative number of ants").
+    wakeUp(G, N-1, [A|Ants]);
+wakeUp(_ , _, _) -> throw("Cannot awake a negative number of ants").
 
 exterminate(Ants) ->
     lists:map(fun(A) -> exit(A,kill) end, Ants).
 
-edgesReplication([],ACC) -> lists:concat(ACC);
-edgesReplication([{NBR,Cost,PF}|NBH],ACC) ->
-    edgesReplication(NBH,[lists:duplicate(PF,{NBR,Cost})|ACC]).
+edgesReplication([], ACC) -> lists:concat(ACC);
+edgesReplication([{NBR, Cost, PF}|NBH], ACC) ->
+    edgesReplication(NBH, [lists:duplicate(PF, {NBR, Cost})|ACC]).
 
-posibleEdges(Edges) -> edgesReplication(Edges,[]).
+posibleEdges(Edges) -> edgesReplication(Edges, []).
 
 chooseOneOf(X) ->
     lists:nth(rand:uniform(length(X)), X).
 
 setNth(I, List, F) -> setNthAux(I, List, F, []).
 
-setNthAux(1, [X|Xs], F, Acc) -> lists:reverse([F(X)|Acc],Xs);
+setNthAux(1, [X|Xs], F, Acc) -> lists:reverse([F(X)|Acc], Xs);
 setNthAux(I, [X|Xs], F, Acc) -> setNthAux(I-1, Xs, F, [X|Acc]).
 
-updateNBH(E, NBH) -> updateNBHAux(E, NBH, []).
+updateNBH(E, NBH, Fun) -> updateNBHAux(E, NBH, Fun, []).
 
-updateNBHAux(_, [], Acc) ->
+updateNBHAux(_, [], _, Acc) ->
     lists:reverse(Acc);
-updateNBHAux(E, [{E, Cost, PF}|NBH], Acc) ->
-    lists:reverse([{E, Cost, PF+1}|Acc], NBH);
-updateNBHAux(E, [{NBR, Cost, PF}|NBH], Acc) ->
-    updateNBHAux(E, NBH, [{NBR, Cost, PF}|Acc]).
+updateNBHAux(E, [{E, Cost, PF}|NBH], Fun, Acc) ->
+    lists:reverse([{E, Cost, Fun(PF)}|Acc], NBH);
+updateNBHAux(E, [{NBR, Cost, PF}|NBH], Fun, Acc) ->
+    updateNBHAux(E, NBH, Fun, [{NBR, Cost, PF}|Acc]).
 
 ask(Master, Ant, Node, Cost) ->
     spawn(
         fun() -> 
             timer:sleep(Cost * 500),
-            Master ! {neighbours,Ant,Node}
+            Master ! {neighbours, Ant, Node}
         end
     ).
 
@@ -64,35 +64,70 @@ ask(Master, Ant, Node, Cost) ->
 %%       List = [Int]
 %%       Int = int()
 %%       PID = pid()
-ant(Master,Node,Visited) ->
+ant(Master, Node, Visited) ->
     receive
         {init} ->
             ask(Master, self(), Node, 0),
             ant(Master, Node, Visited);
-        {goto,L} when is_list(L) ->
+        {goto, L} when is_list(L) ->
             {NewNode,Cost} = chooseOneOf(posibleEdges(L)),
-            io:format("From ~p to ~p~n",[Node,NewNode]), 
+            % io:format("From ~p to ~p~n", [Node,NewNode]),
             timer:sleep(Cost * 500),
-            Master ! {updatePF,Node,NewNode},
+            Master ! {increasePF, Node, NewNode},
             ask(Master, self(), NewNode, Cost),
-            ant(Master,NewNode,Visited);
+            ant(Master, NewNode, Visited);
         _ ->
-            ant(Master,Node,Visited)
+            ant(Master, Node, Visited)
     end.
+
+updatePheromone(From, To, Graph, Fun) ->
+    AuxG =
+        setNth(From, Graph,
+            fun(F) ->
+                updateNBH(To, F, Fun)
+            end),
+    NewG =
+        setNth(To, AuxG,
+            fun(T) ->
+                updateNBH(From, T, Fun)
+            end),
+    NewG.
+
+strengthenPheromone(From, To, AL) ->
+    updatePheromone(From, To, AL, fun(X) -> X + 1 end).
+
+weakenPheromone(From, To, AL) ->
+    updatePheromone(From, To, AL,
+        fun(X) ->
+            if X > 1 ->
+                X - 1;
+            true ->
+                X
+            end
+        end).
+
+evaporatePheromone(Master, From, To) ->
+    spawn(
+        fun() ->
+            timer:sleep(10000),
+            Master ! {decreasePF,From,To}
+        end
+    ).
 
 master(AdjacencyList) ->
     receive
-        {neighbours,Sender,X} ->
+        {neighbours, Sender,X} ->
             Sender ! {goto,lists:nth(X,AdjacencyList)},
             master(AdjacencyList);
-        {updatePF,From,To} ->
-            AuxAL = 
-                setNth(From, AdjacencyList, fun(X) -> updateNBH(To, X) end),
-            NewAL = 
-                setNth(To, AuxAL, fun(X) -> updateNBH(From, X) end),
+        {increasePF, From, To} ->
+            NewAL = strengthenPheromone(From, To, AdjacencyList),
+            evaporatePheromone(self(), From, To),
+            master(NewAL);
+        {decreasePF, From, To} ->
+            NewAL = weakenPheromone(From, To, AdjacencyList),
             master(NewAL);
         {getGraph} ->
-            io:format("Graph: ~p",[AdjacencyList]), 
+            io:format("Graph: ~p", [AdjacencyList]),
             master(AdjacencyList);
         _ ->
             master(AdjacencyList)
